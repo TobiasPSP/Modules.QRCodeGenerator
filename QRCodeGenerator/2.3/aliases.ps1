8cc0c8a34aba421ba30c51e6dd09cd8dcd705a6c
Tobias <T0biii@users.noreply.github.com>
2020-12-09 21:40:32 +0100

    add #3 (New-PSOneQRCodeURI)

--- QRCodeGenerator/2.3/aliases.ps1
+++ QRCodeGenerator/2.3/aliases.ps1
@@ -4,3 +4,4 @@ Set-Alias -Name New-QRCodeWifiAccess -Value New-PSOneQRCodeWifiAccess
 Set-Alias -Name New-QRCodeTwitter -Value New-PSOneQRCodeTwitter
 Set-Alias -Name New-QRCodeVCard -Value New-PSOneQRCodeVCard
 Set-Alias -Name New-QRCodeText -Value New-PSOneQRCodeText
+Set-Alias -Name New-QRCodeURI -Value New-PSOneQRCodeURI
